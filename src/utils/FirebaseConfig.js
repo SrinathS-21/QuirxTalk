54331673bb15e9033a0dcbfc002d20d528f74ff9
Srinath S <154493700+SrinathS-21@users.noreply.github.com>
2024-05-21 15:07:48 +0530

    Update FirebaseConfig.js fb change

--- src/utils/FirebaseConfig.js
+++ src/utils/FirebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk1mTwGyGPX2oy9H5h4DQPlq83q6WYkj8",
-  authDomain: "quirxtalk-2.firebaseapp.com",
-  projectId: "quirxtalk-2",
-  storageBucket: "quirxtalk-2.appspot.com",
-  messagingSenderId: "967299154610",
-  appId: "1:967299154610:web:1abdaf637909769d38ffab",
-  measurementId: "G-ZL9JLEK2P9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
